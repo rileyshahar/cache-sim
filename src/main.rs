1c2dd3e1e1f3afa84c0acb51e7db9e96fac29830
bmorrell <bmorrell@localhost>
2022-06-28 16:24:26 -0700

    Added output for frequency histograms

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
+use std::fs::File;
+use itertools::Itertools;
 
-use cache_sim::{atf::parse, output::to_csv, GeneralModelItem, Trace};
+use cache_sim::{atf::parse, output::to_csv, output::histogram_out, trace::entropy, GeneralModelItem, Trace, NoCondition};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let trace = Trace::from(
@@ -13,6 +15,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let stack_distances = trace.stack_distances();
 
     to_csv("YCSB Sample", &[], &stack_distances)?;
+   	
+   	let histogram = trace.frequency_histogram(&|t: &Trace<_>, i| i > 0 && t[i-1]==t[i]);
+   	let items = trace.iter().unique().copied().collect::<Vec<_>>();
+   	let file = File::create("test.csv")?;
+    
+    histogram_out("NoCondition", &entropy(&histogram), &histogram, &items, file.try_clone()?)?;
+    histogram_out("Test2", &entropy(&histogram), &histogram, &items, file)?;
+    
 
     Ok(())
 }
--- src/output.rs
+++ src/output.rs
@@ -1,8 +1,13 @@
 //! Utilities for outputting data.
 
 use serde::{ser::SerializeSeq, Serialize};
+use std::collections::HashMap;
+use std::io::Write;
+
+
 
 use crate::trace::StackDistance;
+use crate::item::Item;
 
 struct OutputCsvRow<'a> {
     // TODO: does this need to be owned
@@ -54,6 +59,58 @@ pub fn to_csv(
     };
 
     let mut wtr = csv::Writer::from_writer(std::io::stdout());
+
+    wtr.serialize(output)
+}
+
+struct FreqHistRow<'a> {
+    // TODO: does this need to be owned
+    name: &'a str,
+    entropy: &'a f64,
+    frequencies: &'a [u32],
+}
+
+impl Serialize for FreqHistRow<'_> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut seq =
+            serializer.serialize_seq(Some(2 + self.frequencies.len()))?;
+
+        seq.serialize_element(self.name)?;
+        seq.serialize_element(&self.entropy)?;
+        
+        for freq in self.frequencies {
+            seq.serialize_element(freq)?;
+        }
+        seq.end()
+    }
+}
+
+pub fn histogram_out<I: Item, W: Write>(
+    name: &str,
+    entropy: &f64,
+    histogram: &HashMap<I, u32>,
+    items: &Vec<I>,
+    writer: W,
+) -> Result<(), csv::Error> {
+	let mut frequencies = Vec::<u32>::default();
+	for item in items{
+		if let Some(freq) = histogram.get(item){
+			frequencies.push(*freq);
+		}
+		else{
+			frequencies.push(0);
+		}
+	}
+    let output = FreqHistRow {
+        name,
+        entropy,
+        frequencies: &frequencies[..],
+    };
+
+    let mut wtr = csv::Writer::from_writer(writer);
 
     wtr.serialize(output)
 }
